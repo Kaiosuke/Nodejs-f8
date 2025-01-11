0641eac8d224ff073dd23f47e2a011029e77edbc
Kaiosuke <trongleele@gmail.com>
2025-01-11 16:25:37 +0700

    create node

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -7,7 +7,7 @@ const verifyToken = (req, res, next) => {
     if (accessToken) {
       jwt.verify(accessToken, process.env.JWT_ACCESS_TOKEN, (err, user) => {
         if (err) {
-          res.status(403).json("Token is not valid");
+          return res.status(403).json("Token is not valid");
         } else {
           req.user = user;
           next();
@@ -15,7 +15,7 @@ const verifyToken = (req, res, next) => {
       });
     }
   } else {
-    res.status(401).json("You're not authenticated");
+    return res.status(401).json("You're not authenticated");
   }
 };
 
@@ -24,7 +24,7 @@ const verifyAuth = (req, res, next) => {
     if (req.user.role === "admin" || req.user.role === "ceo") {
       next();
     } else {
-      res.status(403).json("You don't have permission to delete");
+      return res.status(403).json("You don't have permission to delete");
     }
   });
 };
---
+++ note.md
@@ -0,0 +1,17 @@
+<!-- Login -->
+
+login: http://localhost:8080/api/auth/login
+register: http://localhost:8080/api/auth/register
+
+<!-- Other -->
+
+getAll: http://localhost:8080/api/products -- Only filter products with deleted: false
+createOne: http://localhost:8080/api/products
+CreateMany: http://localhost:8080/api/products/many
+Update: http://localhost:8080/api/products/:id
+Delete: http://localhost:8080/api/products/product/:id
+Force Delete: http://localhost:8080/api/products/product/:id/force
+
+<!-- Token -->
+
+token: Bearer accessToken
